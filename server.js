c341d7d434bc552e948cc57c40e462888a065a9b
Hunter Loftis <hunter@hunterloftis.com>
2014-12-06 20:08:34 -0800

    running the game server-side

--- server.js
+++ server.js
@@ -1,11 +1,34 @@
 var path = require('path');
 var express = require('express');
+var http = require('http');
+var WSServer = require('ws').Server;
+var Game = require('./lib/game');
 
-express()
+var PORT = process.env.PORT || 5000;
+
+var game = new Game(undefined, undefined);
+var app = express();
+
+app
   .use('/build', express.static(path.join(__dirname, 'build')))
   .get('/', sendIndex)
-  .listen(process.env.PORT || 5000);
+  .listen(PORT);
+
+var server = http.createServer(app);
+var wss = new WSServer({ server: server });
+
+wss.on('connection', onSocketConnection);
+
+game.start();
 
 function sendIndex(req, res, next) {
   res.sendFile(path.join(__dirname, 'index.html'));
+}
+
+function onSocketConnection(ws) {
+  console.log('client connected');
+
+  ws.on('close', function() {
+    console.log('client disconnected');
+  });
 }
