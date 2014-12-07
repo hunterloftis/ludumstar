6604d862d0ffcbf848ba93de958af640540ba060
Hunter Loftis <hunter@hunterloftis.com>
2014-12-06 23:34:43 -0800

    everything working again

--- server.js
+++ server.js
@@ -8,44 +8,45 @@ var present = require('present');
 var PORT = process.env.PORT || 5000;
 var DROP_DELAY = 5000;
 
-var networkRenderer = {
-  clients: []
-  render: function(seconds, state, playerId) {
-    var now = present();
-    var payload = {
-      type: 'state',
-      state: state
-    };
-    var string = JSON.stringify(payload);
+// var networkRenderer = {
+//   clients: [],
+//   render: function(seconds, state, playerId) {
+//     var now = present();
+//     var payload = {
+//       type: 'state',
+//       state: state
+//     };
+//     var string = JSON.stringify(payload);
+//
+//     // First, drop any clients we haven't heard from in a while
+//     this.clients = this.clients.filter(dropIfGone);
+//
+//     // For any non-pending clients, send an update
+//     this.clients.forEach(sendState);
+//
+//     function dropIfGone(client) {
+//       if (client.lastAnswered < now - DROP_DELAY) {
+//         console.log('Dropping client (timed out)');
+//         client.ws.close();
+//         return false;
+//       }
+//       return true;
+//     }
+//
+//     function sendState(client) {
+//       if (client.isPending) return;
+//       client.ws.send(string);
+//       client.isPending = true;
+//     }
+//   }
+// };
 
-    // First, drop any clients we haven't heard from in a while
-    this.clients = this.clients.filter(dropIfGone);
-
-    // For any non-pending clients, send an update
-    this.clients.forEach(sendState);
-
-    function dropIfGone(client) {
-      if (client.lastAnswered < now - DROP_DELAY) {
-        console.log('Dropping client (timed out)');
-        client.ws.close();
-        return false;
-      }
-      return true;
-    }
-
-    function sendState(client) {
-      if (client.isPending) return;
-      client.ws.send(string);
-      client.isPending = true;
-    }
-  }
-};
-
-var game = new Game(networkRenderer, undefined);
+// var game = new Game(networkRenderer, undefined);
 var app = express();
 
 app
   .use('/build', express.static(path.join(__dirname, 'build')))
+  .use('/vendor', express.static(path.join(__dirname, 'vendor')))
   .get('/', sendIndex)
   .listen(PORT);
 
@@ -54,7 +55,7 @@ var wss = new WSServer({ server: server });
 
 wss.on('connection', onSocketConnection);
 
-game.start();
+//game.start();
 
 function sendIndex(req, res, next) {
   res.sendFile(path.join(__dirname, 'index.html'));
