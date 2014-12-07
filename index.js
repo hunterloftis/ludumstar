3e15b4e47875dffb2b007fff89062a17f5a5ecf4
Hunter Loftis <hunter@hunterloftis.com>
2014-12-06 20:32:03 -0800

    state needs to be pure data with no references. damn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports = {
   Renderer: require('./lib/blit-renderer'),
   Keyboard: require('./lib/input/keyboard'),
+  Network: require('./lib/network'),
   Game: require('./lib/game')
 };
--- server.js
+++ server.js
@@ -3,10 +3,45 @@ var express = require('express');
 var http = require('http');
 var WSServer = require('ws').Server;
 var Game = require('./lib/game');
+var present = require('present');
 
 var PORT = process.env.PORT || 5000;
+var DROP_DELAY = 5000;
 
-var game = new Game(undefined, undefined);
+var networkRenderer = {
+  clients: []
+  render: function(seconds, state, playerId) {
+    var now = present();
+    var payload = {
+      type: 'state',
+      state: state
+    };
+    var string = JSON.stringify(payload);
+
+    // First, drop any clients we haven't heard from in a while
+    this.clients = this.clients.filter(dropIfGone);
+
+    // For any non-pending clients, send an update
+    this.clients.forEach(sendState);
+
+    function dropIfGone(client) {
+      if (client.lastAnswered < now - DROP_DELAY) {
+        console.log('Dropping client (timed out)');
+        client.ws.close();
+        return false;
+      }
+      return true;
+    }
+
+    function sendState(client) {
+      if (client.isPending) return;
+      client.ws.send(string);
+      client.isPending = true;
+    }
+  }
+};
+
+var game = new Game(networkRenderer, undefined);
 var app = express();
 
 app
@@ -28,7 +63,17 @@ function sendIndex(req, res, next) {
 function onSocketConnection(ws) {
   console.log('client connected');
 
+  var client = {
+    ws: ws,
+    isPending: false,
+    lastAnswered: present()
+  };
+
+  networkRenderer.clients.push(client);
+
   ws.on('close', function() {
+    var index = networkRenderer.clients.indexOf(client);
+    networkRenderer.client.splice(index, 1);
     console.log('client disconnected');
   });
 }
